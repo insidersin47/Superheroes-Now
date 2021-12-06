2660ad8d0ff118ac19d06765f67c335d1060dddf
insidersin47 <57663101+insidersin47@users.noreply.github.com>
2021-12-06 16:37:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="description">
           
         </p>
-<a href="https://www.dynamicsartsin.com/2021/11/why-mj-not-die-in-spider-man-no-way-home-movie-explanation.html>Reason why MJ wouldn't die in Spider-Man: No Way Home?</a>
+<a href="https://www.dynamicsartsin.com/2021/11/why-mj-not-die-in-spider-man-no-way-home-movie-explanation.html">Reason why MJ wouldn't die in Spider-Man: No Way Home?</a>
       </main>
 
       <Footer />
